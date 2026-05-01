d2c28b0a1486fd04e4550b5ddf60daac9dfdc2a9
DigneshaAmin <digneshaamin@gmail.com>
2026-04-30 23:29:24 -0400

    commit 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,15 +3,44 @@ import json
 
 def emotion_detector(text_to_analyze):
     # URL of the Emotion Detection API
-    URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    URL= 'https://sn-watson-emotion.labs.skills.network/emotion_detection'
 
     
     readtext = { "text": text_to_analyze }
 
     # Send POST request
-    response = requests.post(url, json=reaadtext)
+    response = requests.post(URL, json=readtext)
 
     # Convert response to JSON
-    result = json.loads(response.text)
+    results = json.loads(response.text)
+    if "anger" not in results:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None,
+            "error": results
+        }   
+    anger_score = results['anger']
+    disgust_score = results['disgust']
+    fear_score = results['fear']
+    joy_score = results['joy']
+    sadness_score = results['sadness']
 
-    return result
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    # Add dominant emotion to result
+    results['dominant_emotion'] = dominant_emotion
+
+    return results
+    
